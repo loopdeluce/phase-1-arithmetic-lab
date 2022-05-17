c047d106382b2516334aa3729985871e675cb00a
Lucy <lucy.d.bullen@gmail.com>
2022-05-17 11:27:25 -0600

    commit tree 117f273f9c3b296d4edf6f26a7b1796c0437cec5

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+
+const multiply = num1 * num2;
+
+const random = Math.ceil(Math.random()) * 10;
+
+const num3 = 4;
+const num4 = 10;
+
+const mod = num3 % num4;
+
+const max = Math.max(1, 2, 20, 7, 19);
